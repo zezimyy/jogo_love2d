275a221bbbc397108376f3ed9e7182c91e98da30
André Luis <66038323+zezimyy@users.noreply.github.com>
2023-01-17 18:19:36 +0000

    movimentação do jogador

--- main.lua
+++ main.lua
@@ -15,13 +15,21 @@ function love.load()
 end
 function love.update(dt)
 
+  --Configurando a movimentação do jogador
   if love.keyboard.isDown("w") then
     jogador.y = jogador.y - jogador.velocidade * dt
+  elseif love.keyboard.isDown("s") then
+    jogador.y = jogador.y + jogador.velocidade * dt
+  elseif love.keyboard.isDown("d") then
+    jogador.x = jogador.x + jogador.velocidade * dt
+  elseif love.keyboard.isDown("a") then
+    jogador.x = jogador.x - jogador.velocidade * dt
   end
   
 end
 function love.draw()
 
+  --Criando o jogador 
   love.graphics.circle("fill", jogador.x, jogador.y, jogador.raio)
   
 end
